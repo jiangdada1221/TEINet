05c9bee3eb277691da25e3bdecf0ec5a9845b87d
Yuepeng Jiang <49374730+jiangdada1221@users.noreply.github.com>
2023-03-20 23:30:00 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 from train import *
 from scipy.special import expit
-
+import argparse
 def predict(cdrs,epitopes,labels,model,batch_size = 128):
     '''
     Evaluate the test pairs.
@@ -40,3 +40,30 @@ def predict_only(cdrs,epitopes,model,batch_size = 128):
         score = model(ts,es) 
         y_pres.extend(expit(score.view(-1).detach().cpu().numpy()))
     return y_pres
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--dset_path',type=str) 
+    parser.add_argument('--save_prediction_path',type=str,default='None')
+    parser.add_argument('--use_column',type=str,default='CDR3.beta')
+    parser.add_argument('--batch_size',type=int,default=128)
+    parser.add_argument('--model_path',type=str)
+    parser.add_argument('--device',type=str,default='cuda:0')
+    args = parser.parse_args()
+
+    f = pd.read_csv(args.dset_path)
+    #load model
+    model_tcr = TCRpeg(hidden_size=768,num_layers = 3,load_data=False,embedding_path='encoders/aa_emb_tcr.txt',device=args.device)
+    model_tcr.create_model()
+    model_epi = TCRpeg(hidden_size=768,num_layers = 3,load_data=False,embedding_path='encoders/aa_emb_tcr.txt',device=args.device)
+    model_epi.create_model()
+    model = TEINet(en_tcr=model_tcr,en_epi = model_epi,cat_size=768*2,dropout=0.1,normalize=True,weight_decay = 0,device=args.device).to(args.device)
+    model.load_state_dict(torch.load(args.model_path))
+    # model = torch.load(args.model_path,map_location='cuda:0')
+    cdrs,epitopes,labels = f['CDR3.beta'].values, f['Epitope'].values, f['Label'].values
+    pres = predict_only(cdrs,epitopes,model)
+    print(AUC(labels,pres))
+    with open(args.save_prediction_path,'w') as f:
+        for i in range(len(pres)):
+            f.write(str(pres[i]) + ',' + str(labels[i])+'\n')
+    
